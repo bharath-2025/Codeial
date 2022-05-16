83caf681dcce54e065c4e525e8e2277c9ab6841f
Bharath2025 <mbharath@karunya.edu.in>
2022-05-16 10:01:15 +0530

    Deleting a Post ( Authorized )

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,6 @@
-const Post = require('../models/post')
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
 module.exports.create = function(req,res){
     Post.create({
         content: req.body.content,
@@ -8,4 +10,23 @@ module.exports.create = function(req,res){
 
         return res.redirect('back');
     });
+}
+
+// Creating an Action to delete a post
+module.exports.destroy = function(req,res){
+    //checking whether the post is existed in the database
+    Post.findById(req.params.id,function(err,post){
+        //providing a check for authentication of which user to delete a particular post
+        if(post.user == req.user.id){
+            post.remove();
+
+            // deleting posts will result in deleting all the comments inside that post also. So we are deleting all the comments of that post.
+            Comment.deleteMany({post: req.params.id},function(err){
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+
+    })
 }
